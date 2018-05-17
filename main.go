3b755864baca88e2c300db696d5e1b53da5c7666
K.Lin <l714.100@gmail.com>
2018-05-17 23:48:28 +0800

    GAE: Handling HTML Form Data

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 
-	"google.golang.org/appengine"
-	// Required external App Engine library
+	"google.golang.org/appengine" // Required external App Engine library
 )
+
+var ( //Initialize template variable
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+type templateParams struct { //Define template parameters as a data structure with two fields
+	Notice string
+	Name   string
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// if statement redirects all invalid URLs to the root homepage.
@@ -15,6 +25,28 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
+	// [START handling HTML form data]
+	params := templateParams{}
+	if r.Method == "GET" {
+		indexTemplate.Execute(w, params)
+		return
+	}
+	//It's a POST request, so handle the form submission.
+	name := r.FormValue("name")
+	params.Name = name //Preserve the name field.
+	if name == "" {
+		name = "Anonymous Gopher"
+	}
+	if r.FormValue("message") == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		params.Notice = "No message provided"
+		indexTemplate.Execute(w, params)
+		return
+	}
+	//TODO: save a message into a database
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
+	// [END handling HTML form data]
+	indexTemplate.Execute(w, params)
 }
 
 func main() {
