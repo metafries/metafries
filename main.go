75701616f60d6226cd2a4be0f628e217625c4fd3
K.Lin <l714.100@gmail.com>
2018-06-02 23:27:59 +0800

    NoSQL: Saving a post to Cloud Datastore

--- main.go
+++ main.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"time"
 
 	"google.golang.org/appengine" // Required external App Engine library
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
 
 var ( //Initialize template variable
 	indexTemplate = template.Must(template.ParseFiles("index.html"))
 )
 
-type templateParams struct { //Define template parameters as a data structure with two fields
-	Notice string
-	Name   string
+//Post Define each user-submitted post as a data structure with three fields
+type Post struct {
+	Author  string
+	Message string
+	Posted  time.Time
+}
+
+type templateParams struct { //Define template parameters as a data structure with four fields
+	Notice  string
+	Name    string
+	Message string
+	Posts   []Post
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +37,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
+	ctx := appengine.NewContext(r) //Link all operations related to a given request together
 	// [START handling HTML form data]
 	params := templateParams{}
 	if r.Method == "GET" {
@@ -32,6 +45,11 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	//It's a POST request, so handle the form submission.
+	post := Post{
+		Author:  r.FormValue("name"),
+		Message: r.FormValue("message"),
+		Posted:  time.Now(),
+	}
 	name := r.FormValue("name")
 	params.Name = name //Preserve the name field.
 	if name == "" {
@@ -43,7 +61,18 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		indexTemplate.Execute(w, params)
 		return
 	}
-	//TODO: save a message into a database
+	key := datastore.NewIncompleteKey(ctx, "Post", nil)
+	// [START add_post]
+	if _, err := datastore.Put(ctx, key, &post); err != nil {
+		log.Errorf(ctx, "datastore.Put: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't add new post. Try again?"
+		params.Message = post.Message //Preserve their message so they can try again.
+		indexTemplate.Execute(w, params)
+		return
+	}
+	// [END add_post]
+	params.Posts = append([]Post{post}, params.Posts...) //Prepend the post that was just added.
 	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
 	// [END handling HTML form data]
 	indexTemplate.Execute(w, params)
