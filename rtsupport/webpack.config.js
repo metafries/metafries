a7482e7b59d7bf4a99f6ff487a3b9d300d88c712
K.Lin <l714.100@gmail.com>
2018-10-17 22:29:57 +0800

    Webpack: +Babel Compiler: Customize webpack.config.js

---
+++ rtsupport/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: '../static/bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    }    
+}
