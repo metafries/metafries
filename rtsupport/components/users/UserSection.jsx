83b68cd2af6a73bd026f9b05eb5dc714ad075093
K.Lin <l714.100@gmail.com>
2018-10-26 17:00:04 +0800

    React: Component[s]: Add <UserForm /><UserSection />

---
+++ rtsupport/components/users/UserSection.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import UserForm from './UserForm.jsx';
+import UserList from './UserList.jsx';
+
+class UserSection extends Component {
+    render() {
+        return (
+            <div className='support card'>
+                <div className='card-header bg-primary text-white'>
+                    <strong>Users</strong>
+                </div>
+                <div className='card-body channels bg-secondary'>
+                    <UserList {...this.props} />
+                    <UserForm {...this.props} />                    
+                </div>
+            </div>            
+        )
+    }
+}
+
+UserSection.propTypes = {
+    users: PropTypes.object.isRequired,
+    setUserName: PropTypes.func.isRequired
+}
+
+export default UserSection
