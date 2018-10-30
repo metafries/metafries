28f9555de7714b8b557119ede9fca0e25bc16154
K.Lin <l714.100@gmail.com>
2018-10-30 15:12:04 +0800

    WebSocket: Comms With The Simulated Server + $webpack

--- rtsupport/components/App.jsx
+++ rtsupport/components/App.jsx
@@ -10,17 +10,44 @@ class App extends Component {
             channels: [],
             users: [],
             messages: [],
-            activeChannel: {}
+            activeChannel: {},
+            connected: false
         }
+    }
+    componentDidMount() {
+        let ws = this.ws = new WebSocket('ws://echo.websocket.org');
+        ws.onmessage = this.message.bind(this);
+        ws.onopen = this.open.bind(this);
+        ws.onclose = this.close.bind(this);
+    }
+    message(e) {
+        const event = JSON.parse(e.data);
+        if (event.name === 'channel add') {
+            this.newChannel(event.data);
+        }
+    }
+    open() {
+        this.setState({connected: true});
+    }
+    close() {
+        this.setState({connected: false});
+    }
+    newChannel(channel) {
+        let {channels} = this.state;
+        channels.push(channel);
+        this.setState({channels});
     }
     addChannel(name) {
         let {channels} = this.state;
-        channels.push({
-            id: channels.length,
-            name
-        });
-        this.setState({channels});
         // TODO: Send To Server
+        let msg = {
+            name: 'channel add',
+            data: {
+                id: channels.length,
+                name
+            }
+        }
+        this.ws.send(JSON.stringify(msg));
     }
     setChannel(activeChannel) {
         this.setState({activeChannel});
@@ -63,11 +90,10 @@ class App extends Component {
                     />
                 </div>
                 <div className='col-lg-8'>
-                <MessageSection 
-                    {...this.state}
-                    addMessage={this.addMessage.bind(this)}
-                />
-                
+                    <MessageSection 
+                        {...this.state}
+                        addMessage={this.addMessage.bind(this)}
+                    />                
                 </div>
             </div>
         )
