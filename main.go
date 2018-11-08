672a3a5a295e30b5ba150fe2ee60727402f5e535
K.Lin <l714.100@gmail.com>
2018-11-08 15:46:41 +0800

    Webpack: Remove Client/Server Related Code

--- main.go
+++ main.go
@@ -1,21 +1,141 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
+	"time"
+
+	firebase "firebase.google.com/go"
 
 	"google.golang.org/appengine" // Required external App Engine library
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
 
-type Channel struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+// [START new_variable]
+var ( //Initialize template variable
+	firebaseConfig = &firebase.Config{
+		DatabaseURL:   "https://metafries.firebaseio.com",
+		ProjectID:     "metafries",
+		StorageBucket: "metafries.appspot.com",
+	}
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+// [END new_variable]
+
+//Post Define each user-submitted post as a data structure with three fields
+// [START new_post_field]
+type Post struct {
+	Author  string
+	UserID  string
+	Message string
+	Posted  time.Time
+}
+
+// [END new_post_field]
+
+type templateParams struct { //Define template parameters as a data structure with four fields
+	Notice  string
+	Name    string
+	Message string
+	Posts   []Post
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// if statement redirects all invalid URLs to the root homepage.
+	// Ex: if URL is http://[YOUR_PROJECT_ID].appspot.com/FOO, it will be
+	// redirected to http://[YOUR_PROJECT_ID].appspot.com.
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+		return
+	}
+	ctx := appengine.NewContext(r) //Link all operations related to a given request together
+	// [START handling HTML form data]
+	params := templateParams{}
+	q := datastore.NewQuery("Post").Order("-Posted").Limit(20) //Requests the twenty most recent Post objects in Posted descending order
+	// [START get_posts]
+	if _, err := q.GetAll(ctx, &params.Posts); err != nil {
+		log.Errorf(ctx, "Getting posts: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't get latest posts. Refresh?"
+		indexTemplate.Execute(w, params)
+		return
+	}
+	// [END get_posts]
+	if r.Method == "GET" {
+		indexTemplate.Execute(w, params)
+		return
+	}
+	//It's a POST request, so handle the form submission.
+
+	//[START firebase_token]
+	message := r.FormValue("message")
+
+	errHandler := func() {
+		params.Notice = "Couldn't anthenticate. Try logging in again?"
+		params.Message = message // Preserve their message so they can try again.
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// Create a new Firebase App
+	app, err := firebase.NewApp(ctx, firebaseConfig)
+	if err != nil {
+		errHandler()
+	}
+	// Create a new authenticator for the app.
+	auth, err := app.Auth(ctx)
+	if err != nil {
+		errHandler()
+	}
+	// Verify the token passed in by the uer is valid.
+	tok, err := auth.VerifyIDTokenAndCheckRevoked(ctx, r.FormValue("token"))
+	if err != nil {
+		errHandler()
+	}
+	// Use the validated token to get the user's information.
+	user, err := auth.GetUser(ctx, tok.UID)
+	if err != nil {
+		errHandler()
+	}
+
+	// [START logged_in_post]
+	post := Post{
+		UserID:  user.UID, // Include UserID in case Author isn't unique.
+		Author:  user.DisplayName,
+		Message: message,
+		Posted:  time.Now(),
+	}
+	// [END logged_in_post]
+	params.Name = post.Author
+
+	if post.Message == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		params.Notice = "No message provided"
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	key := datastore.NewIncompleteKey(ctx, "Post", nil)
+	// [START add_post]
+	if _, err := datastore.Put(ctx, key, &post); err != nil {
+		log.Errorf(ctx, "datastore.Put: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't add new post. Try again?"
+		params.Message = post.Message //Preserve their message so they can try again.
+		indexTemplate.Execute(w, params)
+		return
+	}
+	// [END add_post]
+	params.Posts = append([]Post{post}, params.Posts...) //Prepend the post that was just added.
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", post.Author)
+	// [END handling HTML form data]
+	indexTemplate.Execute(w, params)
 }
 
 func main() {
-	// http.HandleFunc("/", indexHandler)
-	router := NewRouter()
-	router.Handle("channel add", addChannel)
-	http.Handle("/", router)
-	http.ListenAndServe(":4000", nil)
+	http.HandleFunc("/", indexHandler)
 	appengine.Main() // Starts the server to receive requests
 }
--- rtsupport/components/App.jsx
+++ rtsupport/components/App.jsx
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
 import UserSection from './users/UserSection.jsx';
 import MessageSection from './messages/MessageSection.jsx';
-import Socket from '../socket.js';
 
 class App extends Component {
     constructor(props) {
@@ -11,80 +10,43 @@ class App extends Component {
             channels: [],
             users: [],
             messages: [],
-            activeChannel: {},
-            connected: false
+            activeChannel: {}
         }
     }
-    componentDidMount() {
-        let socket = this.socket = new Socket();
-        socket.on('connect', this.onConnect.bind(this));
-        socket.on('disconnect', this.onDisconnect.bind(this));
-        socket.on('channel add', this.onAddChannel.bind(this));
-        socket.on('user add', this.onAddUser.bind(this));
-        socket.on('user edit', this.onEditUser.bind(this));
-        socket.on('user remove', this.onRemoveUser.bind(this));
-        socket.on('message add', this.onAddMessage.bind(this));
-    }
-    onAddMessage(message) {
-        let {messages} = this.state;
-        messages.push(message);
-        this.setState({messages});
-    }
-    onRemoveUser(removeUser) {
-        let {users} = this.state;
-        users = users.filter( user => {
-            return user.id !== removeUser.id
-        });
-        this.setState({users});
-    }
-    onAddUser(user) {
-        let {users} = this.state;
-        users.push(user);
-        this.setState({users});
-    }
-    onEditUser(editUser) {
-        let {users} = this.state;
-        users = users.map( user => {
-            if (editUser.id === user.id) {
-                return editUser;
-            }
-            return user;
-        });
-        this.setState({users});
-    }
-    onConnect() {
-        this.setState({connected: true});
-        this.socket.emit('channel subscribe');
-        this.socket.emit('user subscribe');
-    }
-    onDisconnect() {
-        this.setState({connected: false});
-    }
-    onAddChannel(channel) {
-        let {channels} = this.state;
-        channels.push(channel);
-        this.setState({channels});
-    }
     addChannel(name) {
-        this.socket.emit('channel add', data);
+        let {channels} = this.state;
+        channels.push({
+            id: channels.length,
+            name
+        });
+        this.setState({channels});
+        // TODO: Send To Server
     }
     setChannel(activeChannel) {
         this.setState({activeChannel});
-        this.socket.emit('message unsubscribe');
-        this.setState({messages: []});
-        this.socket.emit('message subscribe', {
-            channelId: activeChannel.id
-        });
+        // TODO: Get Chnnels Messages
     }
     setUserName(name) {
-        this.socket.emit('user edit', {name});
+        let {users} = this.state;
+        users.push({
+            id: users.length,
+            name
+        });
+        this.setState({users});
+        // TODO: Send To Server        
     }
     addMessage(body) {
-        let {activeChannel} = this.state;
-        this.socket.emit('message add', {
-            channelId: activeChannel.id,
-            body
+        let {messages, users} = this.state;
+        let createdAt = new Date;
+        let author = users.length > 0 ? users[0].name : 'anonymous';
+        messages.push({
+            id: messages.length,
+            body,
+            createdAt,
+            author
         });
+        this.setState({messages});
+        // TODO: Send To Server        
     }
     render() {
         return (
@@ -101,10 +63,11 @@ class App extends Component {
                     />
                 </div>
                 <div className='col-lg-8'>
-                    <MessageSection 
-                        {...this.state}
-                        addMessage={this.addMessage.bind(this)}
-                    />                
+                <MessageSection 
+                    {...this.state}
+                    addMessage={this.addMessage.bind(this)}
+                />
+                
                 </div>
             </div>
         )
