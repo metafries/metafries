04b67653d59a17f7829ca14dfc04b5782e531814
K.Lin <l714.100@gmail.com>
2018-12-15 19:19:34 +0800

    ELT v2.0 / Redux: Actions + Reducers

--- rtsupport/app/reducers/rootReducer.js
+++ rtsupport/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import testReducer from '../../components/testarea/testReducer.js'
+import eventReducer from '../../components/events/eventReducer.jsx'
 
 const rootReducer = combineReducers({
-    test: testReducer
+    test: testReducer,
+    events: eventReducer
 })
 
 export default rootReducer
