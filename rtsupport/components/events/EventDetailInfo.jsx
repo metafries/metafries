70177ce3718a37d71ec5dfb65bcd48c78b343e82
K.Lin <l714.100@gmail.com>
2018-12-20 00:05:45 +0800

    EDT v0.3 / UIUX Of EDT-I

--- rtsupport/components/events/EventDetailInfo.jsx
+++ rtsupport/components/events/EventDetailInfo.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const EventDetailInfo = () => {
   return (
-    <div>
-      <h1>Info</h1>
-    </div>
+    <table class="table">
+      <tbody>
+        <tr>
+          <th scope="row"><i class="fas fa-info"></i></th>
+          <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.</td>
+        </tr>
+        <tr>
+          <th scope="row"><i class="fas fa-calendar-day"></i></th>
+          <td>2018/03/28, 14:00 - 2018/03/29, 11:00</td>
+        </tr>
+        <tr>
+          <th scope="row"><i class="fas fa-map-marked-alt"></i></th>
+          <td>Lower Kings Road, Brighton BN1 2LN英國</td>
+        </tr>
+      </tbody>      
+    </table>
   )
 }
 
