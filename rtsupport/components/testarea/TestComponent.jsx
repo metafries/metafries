7e5b59296cd2dfc902d5af62290fc6d9325bff91
K.Lin <l714.100@gmail.com>
2019-01-25 22:55:18 +0800

    * / Test of React Places Autocomplete
    https://github.com/hibiken/react-places-autocomplete
    https://github.com/blueberryapps/react-load-script

--- rtsupport/components/testarea/TestComponent.jsx
+++ rtsupport/components/testarea/TestComponent.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Script from 'react-load-script'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import { incrementCounter, decrementCounter } from './testActions.jsx'
 
 const mapState = (state) => ({
@@ -12,14 +14,88 @@ const actions = {
 }
 
 class TestComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      address: '',
+      scriptLoaded: false
+    };
+  }
+
+  handleScriptLoad = () => {
+    this.setState({
+      scriptLoaded: true
+    })
+  }
+
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = (address, placeId) => {
+    this.setState({ address, placeId })
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(error => console.error('Error', error));
+  };
+
   render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange,
+    }
     const { incrementCounter, decrementCounter, data } = this.props
     return (
       <div>
+        <Script
+          url='https://maps.googleapis.com/maps/api/js?key=AIzaSyA8xyoeTTfh5SOxWdF8C5J9oD0PrBQv3WQ&libraries=places'
+          onLoad={this.handleScriptLoad}
+        />
         <h1>Test Area</h1>
         <h3>The answer is: {data}</h3>
         <button onClick={incrementCounter} type='button' className='btn btn-success'>Increment</button>
         <button onClick={decrementCounter} type='button' className='btn btn-danger'>Decrement</button>
+        {this.state.scriptLoaded &&
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: 'Search Places ...',
+                    className: 'location-search-input',
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? 'suggestion-item--active'
+                      : 'suggestion-item';
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+        }
       </div>
     )
   }
