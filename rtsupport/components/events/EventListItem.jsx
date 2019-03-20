76b90b7d35cb0c4cfd3f7742e18454ad7327e404
K.Lin <l714.100@gmail.com>
2019-03-20 22:06:01 +0800

    E1.1 / Redesign <EventListItem/> Info Layout
    Ref. 82e0904

--- rtsupport/components/events/EventListItem.jsx
+++ rtsupport/components/events/EventListItem.jsx
@@ -23,7 +23,28 @@ class EventListItem extends Component {
   render() {
     const {fba, event} = this.props;
     return (
-      <div className='card border-dark rounded-0 mb-3'>
+      <div className='card border-0 rounded-0 mb-3'>
+        <div class="card-header p-0 bg-white">
+          <table class="table mb-0">
+            <tbody>
+              <tr>
+                <th scope="row" className='border-0 float-right px-0'>
+                  <img src={event.hostAvatarUrl} className="hoster" alt="..."/>
+                </th>
+                <td className='border-0'>
+                  <h3 className='mb-0'>{event.hostedBy}</h3>
+                  <small className='font-weight-bold h6'>
+                    {DateTime.fromJSDate(event.createdAt.toDate()).toFormat('FF')}
+                    <strong> · </strong>CREATED A
+                    {event.permission == 0 && <span className='mx-1'>PUBLIC</span>}
+                    {event.permission == 1 && <span className='mx-1'>PRIVATE</span>}
+                    <a href={`/events/${event.id}`}>EVENT</a>
+                  </small>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <div id={event.id} class="carousel slide" data-ride="carousel" data-interval="2000">
           <ol class="carousel-indicators">
             <li data-target={`#${event.id}`} data-slide-to="0" class="active"></li>
@@ -51,44 +72,39 @@ class EventListItem extends Component {
           </div>
         </div>
         <div className='card-body transbox pb-0'>
+          <h4>
+            {event.title}
+          </h4>
+          <h5 className='mb-4'>
+            -- views
+            <strong> · </strong>
+            {event.permission == 0 && <span><i class="fas fa-globe mr-2"></i>Public</span>}
+            {event.permission == 1 && <span><i class="fas fa-lock mr-2"></i>Private</span>}
+            <strong> · </strong>
+            Hosted by <a href='#' className='edh-a'>{event.hostedBy}</a>
+          </h5>
           <table class="table">
             <tbody>
-              <tr>
-                <th scope="row" className='border-0'>
-                  <img src={event.hostAvatarUrl} className="signout float-right" alt="..."/>
-                </th>
-                <td className='border-0'>
-                  <h3>
-                    {event.title}
-                    {
-                      fba.uid == event.hostUid &&
-                      <a href="#" class="badge badge-dark rounded-0 ml-2">HOST</a>
-                    }                  
-                  </h3>
-                  <h5>
-                    {event.permission == 0 && <span><i class="fas fa-globe mr-2"></i>Public</span>}
-                    {event.permission == 1 && <span><i class="fas fa-lock mr-2"></i>Private</span>}
-                    <strong> · </strong>
-                    Hosted by <a href='#' className='edh-a'>{event.hostedBy}</a>
-                  </h5>
-                </td>
-              </tr>
               <tr className='h6'>
                 <th scope="row" className='text-right'>VENUE</th>
                 <td>{event.location}</td>
               </tr>
               <tr className='h6'>
-                <th scope="row" className='text-right'>DATE, TIME</th>
+                <th scope="row" className='text-right'>SCHEDULE</th>
                 <td>
-                  {DateTime.fromJSDate(event.startDate.toDate()).toFormat('yyyy/MM/dd, HH:mm')}
+                  {DateTime.fromJSDate(event.startDate.toDate()).toFormat('ff')}
                   <span className='mx-2'>-</span>
-                  {DateTime.fromJSDate(event.endDate.toDate()).toFormat('yyyy/MM/dd, HH:mm')}
+                  {DateTime.fromJSDate(event.endDate.toDate()).toFormat('ff')}
                 </td>
               </tr>
             </tbody>      
           </table>
+          {
+              fba.uid == event.hostUid &&
+              <h5 className='mb-2'><a href="#" class="badge badge-dark rounded-0 mr-2">HOST</a></h5>
+          }                  
         </div>  
-        <div class="card-footer transbox">
+        <div class="card-footer transbox rounded-0">
           <LinkContainer to={`/events/${event.id}`}>
             <button 
               type="button" 
