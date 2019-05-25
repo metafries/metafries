ff08b38329ecdf1bfce7b0d296b8b458868308c1
K.Lin <l714.100@gmail.com>
2019-05-25 18:05:31 +0800

    U2.4 / Deploy functions[updateStatus(us-central1)] to Firebase
    -> add the newActivity general function
    -> update Node.js 8 function createEvent(us-central1)

--- rtsupport/functions/index.js
+++ rtsupport/functions/index.js
@@ -3,27 +3,64 @@ const admin = require('firebase-admin')
 
 admin.initializeApp(functions.config().firebase)
 
-exports.createEvent = functions.firestore
+const newActivity = (type, target, data) => {
+    return {
+        type,
+        eventId: target,        
+        eventStartDate: data.startDate,
+        eventEndDate: data.endDate,
+        hostedBy: data.hostedBy,
+        title: data.title,
+        hostAvatarUrl: data.hostAvatarUrl,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        hostUid: data.hostUid,
+    }
+}
+
+// Listen for updates to any `event` document.
+exports.updateStatus = functions.firestore
     .document('events/{eventId}')
-    .onCreate(event => {
-        let newEvent = event.data()
-        console.log('events: ', newEvent)
-        const newActivity = {
-            type: 'CREATE_EVENT',
-            eventStartDate: newEvent.startDate,
-            eventEndDate: newEvent.endDate,
-            hostedBy: newEvent.hostedBy,
-            title: newEvent.title,
-            hostAvatarUrl: newEvent.hostAvatarUrl,
-            timestamp: admin.firestore.FieldValue.serverTimestamp(),
-            hostUid: newEvent.hostUid,
-            eventId: event.id,
-        }
-        console.log('activity: ', newActivity)
+    .onUpdate((change, context) => {
+        // Retrieve the current and previous value
+        const events_fs = change.after.data();
+        const prevEvents_fs = change.before.data();
+        console.log({events_fs})
+        console.log({prevEvents_fs})
+
+        // Only update if the status has changed
+        if (events_fs.status === prevEvents_fs.status) return null
+
+        console.log({context})        
+        const activity_fs = newActivity(
+            'UPDATE_STATUS', context.params.eventId, events_fs,
+        )
+        console.log({activity_fs})
+
         return admin
             .firestore()
             .collection('activity')
-            .add(newActivity)
+            .add(activity_fs)
+            .then((docRef) => {
+                return console.log('Activity created with ID: ', docRef.id)
+            })
+            .catch((err) => {
+                return console.log('Error adding activity: ', err)
+            })        
+    })
+
+exports.createEvent = functions.firestore
+    .document('events/{eventId}')
+    .onCreate(e => {
+        const events_fs = e.data()
+        console.log({events_fs})
+        const activity_fs = newActivity(
+            'CREATE_EVENT', e.id, events_fs,
+        )
+        console.log({activity_fs})
+        return admin
+            .firestore()
+            .collection('activity')
+            .add(activity_fs)
             .then((docRef) => {
                 return console.log('Activity created with ID: ', docRef.id)
             })
